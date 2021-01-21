ce9ffe5dad8cc63423a0de7d0724cdf959e2adff
padhisayusuf <padhisaa17@gmail.com>
2021-01-21 13:22:16 +0700

    dashboard kurir

---
+++ app/src/main/java/com/aplikasijagad/DashboardKurir.kt
@@ -0,0 +1,11 @@
+package com.aplikasijagad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardKurir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_kurir)
+    }
+}
