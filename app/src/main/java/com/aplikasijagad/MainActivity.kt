097c8568e05f7716d7def2f24db26767ce81b3e4
padhisayusuf <padhisaa17@gmail.com>
2021-01-21 14:44:50 +0700

    edit MainActivity.kt

--- app/src/main/java/com/aplikasijagad/MainActivity.kt
+++ app/src/main/java/com/aplikasijagad/MainActivity.kt
@@ -2,11 +2,18 @@ package com.aplikasijagad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import com.aplikasijagad.Login.LoginAdmin
+import com.aplikasijagad.Login.LoginKurir
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_kurir.setOnClickListener { startActivity(Intent(this, LoginKurir::class.java)) }
+        btn_admin.setOnClickListener { startActivity(Intent(this, LoginAdmin::class.java)) }
     }
 }
