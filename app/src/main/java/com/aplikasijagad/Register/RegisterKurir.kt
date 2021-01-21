3c0495bb823f239cb609dd756f7b75dddaadf2fa
acer <vironikaayuningtiyas1999@gmail.com>
2021-01-21 09:19:34 +0700

    initial commit

--- app/src/main/java/com/aplikasijagad/Register/RegisterKurir.kt
+++ app/src/main/java/com/aplikasijagad/Register/RegisterKurir.kt
@@ -1,12 +1,12 @@
-package com.aplikasijagad
+package com.aplikasijagad.Register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aplikasijagad.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterKurir : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register_kurir)
     }
 }
