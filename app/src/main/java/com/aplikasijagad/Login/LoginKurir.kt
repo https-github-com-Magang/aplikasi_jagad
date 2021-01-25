67151f11c4111d77bf8a3cd4e02942ca232a3733
acer <vironikaayuningtiyas1999@gmail.com>
2021-01-25 16:04:48 +0700

    baru

--- app/src/main/java/com/aplikasijagad/Login/LoginKurir.kt
+++ app/src/main/java/com/aplikasijagad/Login/LoginKurir.kt
@@ -1,4 +1,12 @@
 package com.aplikasijagad.Login
 
-class LoginKurir {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aplikasijagad.R
+
+class LoginKurir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginkurir)
+    }
 }
--- app/src/main/java/com/aplikasijagad/profile/profileAdmin.kt
+++ app/src/main/java/com/aplikasijagad/profile/profileAdmin.kt
@@ -1,12 +1,12 @@
-package com.aplikasijagad.Login
+package com.aplikasijagad.profile
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.aplikasijagad.R
 
-class LoginAdmin : AppCompatActivity() {
+class profileAdmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loginadmin)
+        setContentView(R.layout.activity_profile_admin)
     }
 }
