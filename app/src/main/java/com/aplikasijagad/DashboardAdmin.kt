a0bf8e9431667e1d509cf434a2f6f56d77934cde
Nurilfebris <nurilfebris42@gmail.com>
2021-01-27 11:26:06 +0700

    First Commmit

--- app/src/main/java/com/aplikasijagad/DashboardAdmin.kt
+++ app/src/main/java/com/aplikasijagad/DashboardAdmin.kt
@@ -3,14 +3,14 @@ package com.aplikasijagad
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.aplikasijagad.add.add_sewamotor
-import kotlinx.android.synthetic.main.activity_dashboard_kurir.*
-import kotlinx.android.synthetic.main.activity_dashboard_sewa.*
+import kotlinx.android.synthetic.main.activity_dashboard_admin.*
+//import kotlinx.android.synthetic.main.activity_dashboard_kurir.*
+//import kotlinx.android.synthetic.main.activity_dashboard_utama_admin.*
 
-class DashboardKurir : AppCompatActivity() {
+class DashboardAdmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_kurir)
+        setContentView(R.layout.activity_dashboard_admin)
         btn_sewa.setOnClickListener { startActivity(Intent(this, DashboardSewa::class.java)) }
     }
 }
--- app/src/main/java/com/aplikasijagad/MainActivity.kt
+++ app/src/main/java/com/aplikasijagad/MainActivity.kt
@@ -3,17 +3,19 @@ package com.aplikasijagad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import com.aplikasijagad.Login.LoginAdmin
-import com.aplikasijagad.Login.LoginKurir
 import com.aplikasijagad.add.add_loket
-import kotlinx.android.synthetic.main.activity_dashboard_kurir.*
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_dashboard_admin.*
+
+//import kotlinx.android.synthetic.main.activity_dashboard_kurir.*
+//import kotlinx.android.synthetic.main.activity_dashboard_utama_admin.*
+//import kotlinx.android.synthetic.main.activity_dashboard_utama_admin.btn_sewa
+//import kotlinx.android.synthetic.main.activity_dashboard_utama_kurir.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_kurir)
+        setContentView(R.layout.activity_dashboard_admin)
 
 ////        btn_kurir.setOnClickListener { startActivity(Intent(this, LoginKurir::class.java)) }
 ////        btn_admin.setOnClickListener { startActivity(Intent(this, LoginAdmin::class.java)) }
