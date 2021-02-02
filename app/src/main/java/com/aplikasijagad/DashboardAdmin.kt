29128b747b836a97e1e7ebd5292328bb9f2197f7
Nurilfebris <nurilfebris42@gmail.com>
2021-02-02 19:04:41 +0700

    berhasil tambah data loket

--- app/src/main/java/com/aplikasijagad/DashboardAdmin.kt
+++ app/src/main/java/com/aplikasijagad/DashboardAdmin.kt
@@ -3,6 +3,7 @@ package com.aplikasijagad
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aplikasijagad.add.add_loket
 import kotlinx.android.synthetic.main.activity_dashboard_admin.*
 //import kotlinx.android.synthetic.main.activity_dashboard_kurir.*
 //import kotlinx.android.synthetic.main.activity_dashboard_utama_admin.*
@@ -12,5 +13,6 @@ class DashboardAdmin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_admin)
         btn_sewa.setOnClickListener { startActivity(Intent(this, DashboardSewa::class.java)) }
+        btn_loket.setOnClickListener { startActivity(Intent(this, add_loket::class.java)) }
     }
 }
