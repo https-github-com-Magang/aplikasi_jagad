e9a4ef12f967db068b81a12104b420f652920c43
acer <vironikaayuningtiyas1999@gmail.com>
2021-02-04 12:55:51 +0700

    coba

--- app/src/main/java/com/aplikasijagad/add/add_loket.kt
+++ app/src/main/java/com/aplikasijagad/add/add_loket.kt
@@ -3,7 +3,6 @@ package com.aplikasijagad.add
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.aplikasijagad.DashboardKurir
 import com.aplikasijagad.R
 import kotlinx.android.synthetic.main.activity_add_loket.*
 
@@ -11,6 +10,6 @@ class add_loket : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_loket)
-        home.setOnClickListener { startActivity(Intent(this, DashboardKurir::class.java)) }
+
     }
 }
