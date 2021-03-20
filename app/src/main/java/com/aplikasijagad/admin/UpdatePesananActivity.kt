fe4e60bf2655aefca0ac7896ea857feef26d3503
acer <vironikaayuningtiyas1999@gmail.com>
2021-03-21 01:05:31 +0700

    update spv fixs

--- app/src/main/java/com/aplikasijagad/admin/UpdatePesananActivity.kt
+++ app/src/main/java/com/aplikasijagad/admin/UpdatePesananActivity.kt
@@ -3,7 +3,13 @@ package com.aplikasijagad.admin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -12,77 +18,81 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aplikasijagad.AdapterUtil
 import com.aplikasijagad.DetailOrderActivity
-//import com.firebase.ui.FirebaseRecyclerAdapter
+
 import com.aplikasijagad.R
 import com.aplikasijagad.databinding.ActivityUpdatePesananBinding
 import com.aplikasijagad.models.Amplop
 import com.aplikasijagad.models.SURATJALAN
-import com.firebase.ui.auth.data.model.User
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.activity_detail_order.*
 import kotlinx.android.synthetic.main.activity_update_pesanan.*
 import kotlinx.android.synthetic.main.activity_update_pesanan.view.*
+import kotlinx.android.synthetic.main.list_amplop.*
 import kotlinx.android.synthetic.main.list_amplop.view.*
+import kotlinx.android.synthetic.main.list_suratjalan.view.*
 
 class UpdatePesananActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUpdatePesananBinding
     lateinit var ref: DatabaseReference
-    lateinit var amplopList: MutableList<Amplop>
-    private lateinit var adapter: AdapterUtil<Amplop>
+    lateinit var amplopList: MutableList<SURATJALAN>
+    private lateinit var adapter: AdapterUtil<SURATJALAN>
+    lateinit var searchtext:EditText
     lateinit var recyclerView: RecyclerView
     private lateinit var database: FirebaseDatabase
-    //lateinit var FirebaseRecyclerAdapter : FirebaseRecyclerAdapter<User , UsersViewHolder>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         amplopList = mutableListOf()
         database = FirebaseDatabase.getInstance()
 
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_update_pesanan)
 
-        binding.btCariData.setOnClickListener {
-            if (ed_searchAmplop.text.isEmpty()){
-                Toast.makeText(applicationContext, "Inputan tidak boleh kosong!!", Toast.LENGTH_SHORT)
-            }else{
-                cariAmplop()
-            }
-        }
+//        binding.btCariData.setOnClickListener {
+//            if (ed_searchAmplop.text.isEmpty()){
+//                Toast.makeText(applicationContext, "Inputan tidak boleh kosong!!", Toast.LENGTH_SHORT)
+//            }else{
+//                cariAmplop()
+//            }
+//        }
+//
+//        recyclerView = findViewById(R.id.rvListAmplop)
 
 
 
-        database.getReference("Amplop").orderByChild("uiDriver").addListenerForSingleValueEvent(object :
-                ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {}
+        database.getReference("SURATJALAN").orderByChild("uiDriver").addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
 
-                override fun onDataChange(p0: DataSnapshot) {
-                    if (p0.exists()) {
-                        for (eventSnapshot in p0.children) {
-                            val data = eventSnapshot.getValue(Amplop::class.java)
-                            data?.let {
-                                amplopList.add(it)
-                            }
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()) {
+                    for (eventSnapshot in p0.children) {
+                        val data = eventSnapshot.getValue(SURATJALAN::class.java)
+                        data?.let {
+                            amplopList.add(it)
                         }
                     }
-
-                    adapter = AdapterUtil(R.layout.list_amplop, amplopList, { itemView, item ->
-                        itemView.tv_rincian.text = item.noamplop
-                        itemView.detail_rincian_penerima.text = item.penerima
-                        itemView.detail_rincian_pengirim.text = item.pengirim
-                        itemView.detail_rincian_berat.text = item.berat
-                        itemView.detail_rincian_jenis.text = item.jenisamplop
-                    }, { _, item ->
-                        val intent = Intent(applicationContext, DetailOrderActivity::class.java)
-//
-                    })
-
-                    rvListAmplop.apply {
-                        adapter = this@UpdatePesananActivity.adapter
-                        layoutManager = LinearLayoutManager(context)
-                    }
                 }
-            })
+
+                adapter = AdapterUtil(R.layout.list_suratjalan, amplopList, { itemView, item ->
+                    itemView.tv_noSTB.text = item.uidSRJ
+                    itemView.tv_tglsurat.text = item.tanggal
+                    itemView.tv_Tujuan.text = item.tujuan
+                    itemView.tv_drive.text = item.driver
+                }, { _, item ->
+                    val intent = Intent(this@UpdatePesananActivity, DetailOrderActivity::class.java)
+                    intent.putExtra("data", item)
+                    startActivity(intent)
+                })
+
+                rvListAmplop.apply {
+                    adapter = this@UpdatePesananActivity.adapter
+                    layoutManager = LinearLayoutManager(context)
+                }
+            }
+        })
 
     }
 
@@ -95,7 +105,8 @@ class UpdatePesananActivity : AppCompatActivity() {
 
         val mAlertDialog = mBuilder.show()
 
-        val noamplop = mDialogView.findViewById<TextView>(R.id.tv_rincian)
+        val tv_rincian1 = mDialogView.findViewById<TextView>(R.id.tv_rincian1)
+
 
         FirebaseDatabase.getInstance().reference
             .child("Amplop")
@@ -105,12 +116,11 @@ class UpdatePesananActivity : AppCompatActivity() {
                 }
 
                 override fun onDataChange(p0: DataSnapshot) {
-                    var map = p0.children.first().value as Map<String, Int>
-                    noamplop.text = map["noamplop"].toString()
+                    var map = p0.children.first().value as Map<String, String>
+                    tv_rincian1.text = map["noamplop"].toString()
+
                 }
             })
-        mDialogView.bt_cari_data.setOnClickListener {
-            mAlertDialog.dismiss()
     }
-        }
+
 }
